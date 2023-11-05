fa83cd3419bda1c11319cdcaa10ddfd9ba5d3e98
bojeans <chris.d.anderson87@gmail.com>
2023-11-05 14:29:02 +1300

    introduced pagination feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Products from "./components/Products";
 import fetchAPI from "./components/fetchAPI";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<any[]>([]);
+  const productsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchProducts = async () => {
+    const data = await fetchAPI();
+    setProducts(data);
+  };
+
+  const totalPages = Math.ceil(products.length / productsPerPage);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await fetchAPI();
-      setProducts(data);
-    };
-
     fetchProducts();
   }, []);
 
   return (
-    <div className="App">
-      <h1>Mock shop for demonstration</h1>
-      <Products products={products} />
+    <div className="app-container">
+      <h1>Mock Shop for coding demonstration</h1>
+      <div className="products-container">
+        <Products products={currentProducts} />
+      </div>
+      <div className="pagination">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="pagination-button"
+        >
+          Prev
+        </button>
+        <span className="pagination-info">{`Page ${currentPage} of ${totalPages}`}</span>
+        <button
+          onClick={nextPage}
+          disabled={currentPage === totalPages}
+          className="pagination-button"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
