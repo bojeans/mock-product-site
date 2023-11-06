8f11b4e5c3e26bbfe3d7ccaf1a402e4b4f54d5f2
bojeans <chris.d.anderson87@gmail.com>
2023-11-06 14:14:07 +1300

    modify filter function

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,90 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Products from "./components/Products";
 import fetchAPI from "./components/fetchAPI";
+import SortBy from "./components/sortBy";
+import FilterBy from "./components/filterBy";
 
-const App = () => {
+const App: React.FC = () => {
   const [products, setProducts] = useState<any[]>([]);
-  const productsPerPage = 6;
+  const [sortedProducts, setSortedProducts] = useState<any[]>([]);
+  const [filter, setFilter] = useState<string>("");
   const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 6;
 
   const fetchProducts = async () => {
     const data = await fetchAPI();
     setProducts(data);
+    setSortedProducts(data);
   };
 
-  const totalPages = Math.ceil(products.length / productsPerPage);
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct
-  );
-
-  const nextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
+  const handleSortChange = (value: string) => {
+    let sorted = [...sortedProducts];
+    if (value === "lowprice") {
+      sorted = [...sortedProducts].sort((a, b) => a.price - b.price);
+    } else if (value === "highprice") {
+      sorted = [...sortedProducts].sort((a, b) => b.price - a.price);
+    } else if (value === "category") {
+      sorted = [...sortedProducts].sort((a, b) =>
+        a.category.localeCompare(b.category)
+      );
+    } else if (value === "rating") {
+      sorted = [...sortedProducts].sort(
+        (a, b) => b.rating.rate - a.rating.rate
+      );
     }
+    setSortedProducts(sorted);
   };
 
-  const prevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
+  const handleFilterChange = (value: string) => {
+    setFilter(value);
+    setCurrentPage(1);
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
+  useEffect(() => {
+    const updatedProducts = filter
+      ? products.filter(
+          (product) =>
+            product.category === filter ||
+            (filter === "rating" && product.rating.rate >= 4)
+        )
+      : products;
+    setSortedProducts(updatedProducts);
+  }, [filter, products]);
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = sortedProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="app-container">
-      <h1>Mock Shop for coding demonstration. </h1>
-      <span>
-        *product information fetched from{" "}
-        <a
-          href="https://fakestoreapi.com/docs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          fakestoreAPI
-        </a>{" "}
-        Here's a link to my{" "}
-        <a
-          href="https://github.com/bojeans/mock-product-site"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github Repo
-        </a>
-      </span>
-
+      <h1>Title - woohoo</h1>
+      <div className="filter-sort">
+        <SortBy handleSortChange={handleSortChange} />
+        <FilterBy handleFilterChange={handleFilterChange} />
+      </div>
       <div className="products-container">
         <Products products={currentProducts} />
       </div>
       <div className="pagination">
-        <button
-          onClick={prevPage}
-          disabled={currentPage === 1}
-          className="pagination-button"
-        >
-          Prev
-        </button>
-        <span className="pagination-info">{`Page ${currentPage} of ${totalPages}`}</span>
-        <button
-          onClick={nextPage}
-          disabled={currentPage === totalPages}
-          className="pagination-button"
-        >
-          Next
-        </button>
+        {Array.from({
+          length: Math.ceil(sortedProducts.length / productsPerPage),
+        }).map((_, index) => (
+          <button key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
       </div>
     </div>
   );
